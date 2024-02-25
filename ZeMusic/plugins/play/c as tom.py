c0fd9d3b76bc554a4a055be6d8db51763972d42e
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-25 02:16:00 +0200

    Update c as tom.py

--- ZeMusic/plugins/play/c as tom.py
+++ ZeMusic/plugins/play/c as tom.py
@@ -7,9 +7,9 @@ from ZeMusic.core.call import Mody
 from ZeMusic.utils.database import *
 from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError
 
-@app.on_message(filters.regex("اسم المساعد"))
+@app.on_message(filters.regex("تغير اسم المساعد"))
 async def tom_name(client, message):
-    assistant = await group_assistant(Anon, message.chat.id)
+    assistant = await group_assistant(Mody, message.chat.id)
     await message.reply("ارسل اسم المساعد الجديد:")
     try:
         new_name = await client.ask(message.chat.id, "اكتب اسم المساعد الجديد:")
